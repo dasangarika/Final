0e7e2d4890de98f2114fadd1f5fa4db264e04a46
dasangarika <9angarika@gmail.com>
2020-04-26 20:50:42 +0530

    Updates

---
+++ precache-manifest.a90bad01de307a225b0ffc53332ca3b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4767d1bb8b4f6a94e0e3d66cd76e2e29",
+    "url": "/Final/index.html"
+  },
+  {
+    "revision": "807ebf36f052588af6f4",
+    "url": "/Final/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "8597cdaacc5c3d40d41a",
+    "url": "/Final/static/css/main.71ae691c.chunk.css"
+  },
+  {
+    "revision": "807ebf36f052588af6f4",
+    "url": "/Final/static/js/2.fb9166c9.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/Final/static/js/2.fb9166c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8597cdaacc5c3d40d41a",
+    "url": "/Final/static/js/main.866e2f16.chunk.js"
+  },
+  {
+    "revision": "15b4ed30a6fd793c8d97",
+    "url": "/Final/static/js/runtime-main.cba0e0f2.js"
+  }
+]);
